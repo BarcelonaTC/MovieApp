f98545053512c636c0d05bee1bd22269b8e1c288
abdelrhman1002 <abdelrhmanshabana999@gmail.com>
2025-09-10 18:05:02 +0300

    refactor: update My Lists screen UI and add cancel button functionality

--- app/src/main/java/com/karrar/movieapp/ui/myList/myListUIState/MyListUIEvent.kt
+++ app/src/main/java/com/karrar/movieapp/ui/myList/myListUIState/MyListUIEvent.kt
@@ -5,4 +5,6 @@ sealed interface MyListUIEvent {
     object CLickAddEvent : MyListUIEvent
     data class OnSelectItem(val createdListUIState: CreatedListUIState) : MyListUIEvent
     data class DisplayError(val errorMessage: String) : MyListUIEvent
+
+    object CancelClicked : MyListUIEvent
 }
